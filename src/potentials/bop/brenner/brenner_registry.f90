82a6a08c35943df079a9d61b254a294dc287bab2
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2013-08-07 13:44:13 +0200

    Updated GPL headers.

--- src/potentials/bop/brenner/brenner_registry.f90
+++ src/potentials/bop/brenner/brenner_registry.f90
@@ -1,12 +1,23 @@
 !! ======================================================================
 !! Atomistica - Interatomic potential library
 !! https://github.com/pastewka/atomistica
-!! Lars Pastewka, lars.pastewka@iwm.fraunhofer.de, and others
-!! See the AUTHORS file in the top-level MDCORE directory.
+!! Lars Pastewka, lars.pastewka@iwm.fraunhofer.de, and others.
+!! See the AUTHORS file in the top-level Atomistica directory.
 !!
 !! Copyright (2005-2013) Fraunhofer IWM
-!! This software is distributed under the GNU General Public License.
-!! See the LICENSE file in the top-level MDCORE directory.
+!!
+!! This program is free software: you can redistribute it and/or modify
+!! it under the terms of the GNU General Public License as published by
+!! the Free Software Foundation, either version 2 of the License, or
+!! (at your option) any later version.
+!!
+!! This program is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!! GNU General Public License for more details.
+!!
+!! You should have received a copy of the GNU General Public License
+!! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !! ======================================================================
 
   subroutine REGISTER_FUNC(this, cfg, m)
