f784262f0338671c117dc21ea78cc3cc71a6fa71
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2013-10-10 14:00:38 +0200

    Added collection of useful code snippets.

---
+++ src/python/atomistica/snippets.py
@@ -0,0 +1,43 @@
+# ======================================================================
+# Atomistica - Interatomic potential library
+# https://github.com/pastewka/atomistica
+# Lars Pastewka, lars.pastewka@iwm.fraunhofer.de, and others.
+# See the AUTHORS file in the top-level Atomistica directory.
+#
+# Copyright (2005-2013) Fraunhofer IWM
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ======================================================================
+
+"""
+Code snippets that make life a little easier.
+"""
+
+import numpy as np
+from numpy.linalg import inv
+
+###
+
+def mic(dr, cell):
+    """
+    Apply minimum image convention to an array of distance vectors.
+    """
+    # Check where distance larger than 1/2 cell. Particles have crossed
+    # periodic boundaries then and need to be unwrapped.
+    rec = np.linalg.inv(cell)
+    dri = np.around(np.dot(dr, rec))
+
+    # Unwrap
+    return dr-np.dot(dri, cell)
+
