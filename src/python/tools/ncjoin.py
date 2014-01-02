dfe5c4d77ce369179a8ba77c7e6a57ff849301bb
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-01-02 16:03:58 +0100

    Updated ncjoin.py tool: The script now detects if NetCDF files are consecutive and will fail if they are not. Additionally, if the first argument is a number, this number is taken as an output (time) interval. This allows to sparsify trajectory files and copy only a certain subset of frames.

--- src/python/tools/ncjoin.py
+++ src/python/tools/ncjoin.py
@@ -23,7 +23,7 @@
 # ======================================================================
 
 """
-Join a number of NetCDF trajectory files into a single one.
+Join a individual NetCDF trajectory files into a single one.
 """
 
 import os
@@ -41,30 +41,136 @@ ATOM_DIM = 'atom'
 
 ###
 
+def get_nearest_indices(time, every):
+    """
+    Given a list of times, return indices that are most closely to evenly
+    spaced time intervals of length *every*.
+    """
+
+    r = [ ]
+    m = int(np.max(time)/every)+1
+    last_j = -1
+    for i in range(m):
+        difftime = np.abs(time-i*every)
+        j = np.argmin(difftime)
+        if difftime[j] < 1.0 and j != last_j:
+            r += [ j ]
+            last_j = j
+    return np.array(r)
+
+###
+
+def open_trajs(trajfns, time_var='time', test_var='coordinates', test_tol=1e-6):
+    """
+    Open multiple NetCDF trajectory files and check that they are in order.
+    Remove overlap if files overlap. Returns list of tuples. Tuple contains
+        filename, netCDF4 Dataset object, first frame, last frame
+    """
+
+    data_f = zip(trajfns, map(Dataset, trajfns))
+    filtered_data_f = [ ]
+
+    last_time = None
+    for i in range(len(data_f)-1):
+        fn1, data1 = data_f[i]
+        fn2, data2 = data_f[i+1]
+
+        test_first2 = data2.variables[test_var][0]
+
+        last1 = len(data1.variables[time_var])-1
+        while last1 >= 0 and np.any(np.abs(data1.variables[test_var][last1] - test_first2) > test_tol):
+            print 'Frame %i of %s does not agree with first frame of %s. Checking frame %i.' % ( last1, fn1, fn2, last1-1 )
+            last1 -= 1
+
+        if last1 < 0:
+            raise RuntimeError('%s and %s are not consecutive.' % ( fn1, fn2 ))
+
+        data_slice = slice(0, last1)
+        time = data1.variables[time_var][data_slice]
+        if last_time is not None:
+            time += last_time - time[0]
+        filtered_data_f += [ ( fn1, data1, data_slice, time ) ]
+        last_time = data1.variables[time_var][last1]
+
+    time = data2.variables[time_var]
+    if last_time is not None:
+        time += last_time - time[0]
+    filtered_data_f += [ ( fn2, data2, slice(0, len(time)), time ) ]
+
+    return filtered_data_f
+
+###
+
+def filter_trajs(idata_f, every):
+    # Create a list of all frames
+    idata_oframe = reduce(lambda a,b: a+b, [ [ ( fn, data, i, time[i] ) for i in range(len(time))[data_slice] ] for fn, data, data_slice, time in idata_f ], [])
+
+    # Get indices that corresponds to roughly equally spaced time slots
+    i = get_nearest_indices(np.array([time for fn, data, i, time in idata_oframe]), every)
+    if len(i) == 0:
+        raise RuntimeError('No frames left after filtering.')
+    else:
+        idata_oframe = [ idata_oframe[_i] for _i in i ]
+
+    # Consolidate into a list that contains per-file information
+    filtered_idata_f = [ ]
+    cur_fn, cur_data, cur_slice, cur_time = idata_oframe[0]
+    cur_slice = [ ]
+    cur_time = [ ]
+    for fn, data, i, time in idata_oframe:
+        if data is not cur_data:
+            filtered_idata_f += [ ( cur_fn, cur_data, np.array(cur_slice),
+                                    np.array(cur_time) ) ]
+            cur_fn = fn
+            cur_data = data
+            cur_slice = [ ]
+            cur_time = [ ]
+        cur_slice += [ i ]
+        cur_time += [ time ]
+    filtered_idata_f += [ ( cur_fn, cur_data, np.array(cur_slice),
+                            np.array(cur_time) ) ]
+    return filtered_idata_f
+
+###
+
 if os.path.exists('traj.nc'):
     raise RuntimeError('traj.nc exists already.')
 
+every = None
+if not os.path.exists(sys.argv[1]):
+    # File does not exists, assume this is dump interval
+    every = float(sys.argv[1])
+    trajfns = sys.argv[2:]
+else:
+    trajfns = sys.argv[1:]
+
+if len(trajfns) == 0:
+    raise RuntimeError('Please provide one or more files to concatenate.')
+
+# Open input files and filter if requested
+print 'Opening files and checking file order...'
+idata_f = open_trajs(trajfns)
+if every is not None:
+    idata_f = filter_trajs(idata_f, every)
+
+# Create output file
 odata = Dataset('traj.nc', 'w', clobber=False, format='NETCDF3_64BIT')
-idata_f = map(Dataset, sys.argv[1:])
-idata_f.sort(key=lambda x: x.variables['time'][0])
 
-### Global attributes
 
-for attr_str in idata_f[0].ncattrs():
+### Copy global attributes
+
+for attr_str in idata_f[0][1].ncattrs():
     print "> creating global attribute '%s'..." % attr_str
-    odata.setncattr(attr_str, idata_f[0].getncattr(attr_str))
+    odata.setncattr(attr_str, idata_f[0][1].getncattr(attr_str))
 
-### Copy stuff
+
+### Copy everything else
 
 cursor = 0
-last_time = -1.0
 last_data = None
-for i, idata in enumerate(idata_f):
-    print "Appending '%s' starting at frame %i..." % ( sys.argv[i+1], cursor )
-    time = idata.variables['time'][:]
-    j = 0
-    while time[j] < last_time+TIME_TOL:
-        j += 1
+for trajfn, idata, data_slice, time in idata_f:
+    print "Appending '%s' starting at frame %i..." % ( trajfn, cursor )
+    print 'File contains %i relevant time slots: ' % len(time), time
 
     for var_str, var in idata.variables.iteritems():
         if var_str not in odata.variables:
@@ -83,19 +189,26 @@ for i, idata in enumerate(idata_f):
                 print "> creating attribute '%s' of variable '%s'..." % ( attr_str, var_str )
                 ovar.setncattr(attr_str, var.getncattr(attr_str))
 
-        print "Copying variable '%s'..." % var_str
         if var.dimensions[0] == FRAME_DIM:
-            odata.variables[var_str][cursor:] = var[j:]
+            print "Copying variable '%s'..." % var_str
+            if var_str == 'time':
+                var_data = time
+            else:
+                var_data = var[data_slice]
+            odata.variables[var_str][cursor:] = var_data
         else:
             if not last_data:
+                print "Copying variable '%s'..." % var_str
                 odata.variables[var_str][:] = var[:]
             else:
+                print "Checking variable '%s' for consistency across files..." % var_str
                 if np.any(last_data.variables[var_str][:] != var[:]):
                     raise RuntimeError("Data for per-file variable '%s' "
                                        "differs in '%s' and '%s'." % 
-                                       ( var_str, sys.argv[i], sys.argv[i+1] ))
+                                       ( var_str, trajfns[i-1], trajfns[i] ))
 
-    cursor += len(idata.dimensions[FRAME_DIM])-j
+    cursor += len(time)
+    last_time = time[-1]
     if last_data:
         last_data.close()
     last_data = idata
