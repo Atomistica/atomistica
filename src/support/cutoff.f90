69b2806d77fbb8660192a81ac1b00140680b3931
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-03-10 17:21:46 -0600

    Added cutoff function module. Currently contains trigonometric and exponential cutoff.

---
+++ src/support/cutoff.f90
@@ -0,0 +1,171 @@
+!! ======================================================================
+!! Atomistica - Interatomic potential library
+!! https://github.com/pastewka/atomistica
+!! Lars Pastewka, lars.pastewka@iwm.fraunhofer.de, and others.
+!! See the AUTHORS file in the top-level Atomistica directory.
+!!
+!! Copyright (2005-2013) Fraunhofer IWM
+!!
+!! This program is free software: you can redistribute it and/or modify
+!! it under the terms of the GNU General Public License as published by
+!! the Free Software Foundation, either version 2 of the License, or
+!! (at your option) any later version.
+!!
+!! This program is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!! GNU General Public License for more details.
+!!
+!! You should have received a copy of the GNU General Public License
+!! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!! ======================================================================
+
+!>
+!! Different cutoff functions
+!<
+
+module cutoff
+  use system_module
+  use units_module
+
+  implicit none
+
+  private
+
+  public :: trig_cutoff_t
+  type trig_cutoff_t
+     real(DP) :: r1, r2, fac
+  endtype trig_cutoff_t
+
+  public :: exp_cutoff_t
+  type exp_cutoff_t
+     real(DP) :: r1, r2, fac1, fac2, c, d, off
+  endtype exp_cutoff_t
+
+  public :: init
+  interface init
+     module procedure trig_cutoff_init, exp_cutoff_init
+  endinterface
+
+  public :: fc
+  interface fc
+     module procedure trig_cutoff_f, exp_cutoff_f
+  endinterface fc
+
+contains
+
+  !>
+  !! Initialize trigonometric cutoff
+  !<
+  elemental subroutine trig_cutoff_init(this, r1, r2)
+    implicit none
+
+    type(trig_cutoff_t), intent(out) :: this
+    real(DP),            intent(in)  :: r1, r2
+
+    ! ---
+
+    this%r1  = r1
+    this%r2  = r2
+    this%fac = PI/( this%r2 - this%r1 )
+
+  endsubroutine trig_cutoff_init
+
+
+  !>
+  !! This if f(x)=0.5(1+cos(pi*x)).
+  !! Function is differentiable once.
+  !<
+  subroutine trig_cutoff_f(this, r, val, dval)
+    implicit none
+
+    type(trig_cutoff_t), intent(in)  :: this
+    real(DP),            intent(in)  :: r
+    real(DP),            intent(out) :: val, dval
+
+    ! ---
+
+    real(DP) :: x
+
+    ! ---
+
+    if (r < this%r1) then
+       val  = 1.0_DP
+       dval = 0.0_DP
+    else if (r > this%r2) then
+       val  = 0.0_DP
+       dval = 0.0_DP
+    else
+       x    = this%fac*( r - this%r1 )
+       val  = 0.5_DP *  ( 1.0_DP + cos( x ) )
+       dval = -0.5_DP * this%fac * sin( x )
+    endif
+
+  endsubroutine trig_cutoff_f
+
+
+  !>
+  !! Initialize exponential cutoff
+  !<
+  elemental subroutine exp_cutoff_init(this, r1, r2)
+    implicit none
+
+    type(exp_cutoff_t), intent(out) :: this
+    real(DP),           intent(in)  :: r1, r2
+
+    ! ---
+
+    real(DP) :: val1, dval1, ddval1
+
+    ! ---
+
+    this%r1   = r1
+    this%r2   = r2
+    this%fac1 = 1.0_DP/( this%r2 - this%r1 )
+    val1      = exp(-8.0_DP)
+    dval1     = -24*val1
+    ddval1    = -48*val1-24*dval1
+    this%c    = (-3*dval1+ddval1)/3
+    this%d    = (2*dval1-ddval1)/4
+    this%fac2 = 1.0_DP/(1-val1-this%c-this%d)
+    this%off  = val1+this%c+this%d
+
+  endsubroutine exp_cutoff_init
+
+
+  !>
+  !! This is f(x)=exp(-8*x**3), but corrected such that function, first
+  !! and second derivative go to zero at x=1.
+  !! Function is differentiable twice.
+  !<
+  subroutine exp_cutoff_f(this, r, val, dval)
+    implicit none
+
+    type(exp_cutoff_t), intent(in)  :: this
+    real(DP),           intent(in)  :: r
+    real(DP),           intent(out) :: val, dval
+
+    ! ---
+
+    real(DP) :: x, x2
+
+    ! ---
+
+    if (r < this%r1) then
+       val  = 1.0_DP
+       dval = 0.0_DP
+    else if (r > this%r2) then
+       val  = 0.0_DP
+       dval = 0.0_DP
+    else
+       x      = this%fac1*( r-this%r1 )
+       x2     = x*x
+       val    = exp(-8*x*x2)
+       dval   = -24*x2*val
+       dval   = this%fac1*this%fac2*(dval+3*this%c*x2+4*this%d*x*x2)
+       val    = this%fac2*(val+this%c*x*x2+this%d*x2*x2-this%off)
+    endif
+
+  endsubroutine exp_cutoff_f
+
+endmodule cutoff
