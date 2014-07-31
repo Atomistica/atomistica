9eaf0d0e7197fed376fe462b938106490f788452
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2014-07-31 14:32:27 +0200

    Added surface energy test.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -27,6 +27,7 @@ import unittest
 from neighbor_list import *
 from pbc import *
 from bulk_properties import *
+from surface_properties import *
 from forces_and_virial import *
 from rebo2_molecules import *
 from mio import *
