9ea4ca8b04dd9160ae79f603f14115fed830c4e6
Lars Pastewka <lars.pastewka@iwm.fraunhofer.de>
2015-01-21 17:09:13 +0100

    Need to check for > 0.

--- src/potentials/eam/tabulated_alloy_eam.f90
+++ src/potentials/eam/tabulated_alloy_eam.f90
@@ -386,7 +386,7 @@ contains
     do i = 1, p%natloc
        if (.not. present(mask) .or. mask(i) /= 0) then
           eli = p%el(i)
-          if (IS_EL2(this%els, eli) .and. this%el2db(eli)) then
+          if (IS_EL2(this%els, eli) .and. this%el2db(eli) > 0) then
              maxneb = max(maxneb, nl%last(i)-nl%seed(i)+1)
           endif
        endif
