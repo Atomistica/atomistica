f2010fe2d2f3bbadbc7dd4cec3b427148406e996
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2018-04-10 12:45:28 +0200

    EXAMPLES: Added demonstration of how to extract the electronic structure to SCC DFTB example.

--- examples/ASE/scc_dftb.py
+++ examples/ASE/scc_dftb.py
@@ -1,5 +1,8 @@
 import os
 
+import numpy as np
+np.set_printoptions(precision=2, linewidth=120, suppress=True)
+
 from ase.build import molecule
 
 import atomistica.native as native
@@ -7,7 +10,8 @@ from atomistica import Atomistica
 
 ###
 
-a = molecule('H2O')
+molstr = 'CO2'
+a = molecule(molstr)
 a.center(vacuum=10.0)
 
 ###
@@ -33,17 +37,65 @@ calc = Atomistica(
     )
 
 a.set_calculator(calc)
-print('potential energy =', a.get_potential_energy())
+print('{}, potential energy: {} eV'.format(molstr, a.get_potential_energy()))
 
-print('eigenvalue, occupation')
-for ev, occ in zip(tb.eigenvalues, tb.occupation):
-    print(ev, occ)
+### Extract electronic structure and print some properties
 
-print('Hamiltonian matrix')
-print(tb.Hamiltonian_matrix)
+potential = calc.get_electrostatic_potential()
+charges = calc.get_charges()
+charges0 = tb.neutral_charges
+norb = tb.number_of_orbitals
 
-print('overlap matrix')
-print(tb.overlap_matrix)
+print()
+print('Atom, i  Number of orbitals  Charge, q  q of neutral atom, q0  Potential, phi')
+print('=======  ==================  =========  =====================  ==============')
+for i, (n, q, q0, phi) in enumerate(zip(norb, charges, charges0, potential)):
+    print('{:7} {:19} {:10.2f} {:22.2f} {:15.2f}'.format(i, n, q, q0, phi))
+print('                                sum(q)                           sum(q*phi)/2')
+print('                            {:10.2f}                        {:15.2f} eV'.format(charges.sum(), (charges*potential).sum()/2))
 
-print('density matrix')
-print(tb.density_matrix)
+# Get electronic structure information from the tight-binding calculator
+f = tb.occupation
+dm = tb.density_matrix
+evecs = tb.eigenvectors
+evals = tb.eigenvalues
+H0 = tb.Hamiltonian_matrix
+S = tb.overlap_matrix
+
+print()
+print('Eigenvalue, e  Occupation, f')
+print('=============  =============')
+for ev, occ in zip(evals, f):
+    print('{:13.2f} {:14.2f}'.format(ev, occ))
+print('     sum(f*e)         sum(f)')
+print('{:13.2f} eV {:11.2f}'.format((f*evals).sum(), f.sum()))
+
+print()
+print('Hamiltonian matrix (*without* electrostatic contribution), H0')
+print('=============================================================')
+print(H0)
+print()
+print('Overlap matrix, S')
+print('=================')
+print(S)
+print()
+print('Density matrix, rho')
+print('===================')
+print(dm)
+print()
+
+# Construct effective Hamiltonian
+phiarr = np.ravel(list([_phi]*_n for _phi, _n in zip(potential, norb)))
+phimat = phiarr.reshape(1, -1) + phiarr.reshape(-1, 1)
+Heff = H0 - S*phimat/2
+
+print('Derived quantities')
+print('==================')
+print('sum(f)                                    = {:10.2f}'.format(f.sum()))
+print('tr(S.rho)                                 = {:10.2f}      [should equal sum(f)]'.format(np.trace(S.dot(dm))))
+print('tr(H0.rho)                                = {:10.2f} eV'.format(np.trace(H0.dot(dm))))
+print('tr(H0.rho) + sum(q*phi)/2                 = {:10.2f} eV   [this is the potential energy]'.format(np.trace(H0.dot(dm)) + (charges*potential).sum()/2))
+print('sum(f*e)                                  = {:10.2f}'.format((f*evals).sum()))
+print('tr(Heff.rho)                              = {:10.2f} eV   [should equal sum(f*e)]'.format(np.trace(Heff.dot(dm))))
+print('tr(Heff.rho) - sum(q*phi)/2 - sum(q0*phi) = {:10.2f} eV.  [should equal tr(H0.rho) + sum(q*phi)/2]' \
+  .format(np.trace(Heff.dot(dm)) - (charges*potential).sum()/2 - (charges0*potential).sum()))
