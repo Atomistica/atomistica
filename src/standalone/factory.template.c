d132b8dd7090d384752fde23ca9781c46b3b949c
Andreas Klemenz <andreas.klemenz@iwm.fraunhofer.de>
2025-06-11 15:54:54 +0200

    MAINT: Fixes for compiling with Intel 2025 compilers

--- src/standalone/factory.template.c
+++ src/standalone/factory.template.c
@@ -19,6 +19,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
    ====================================================================== */
 #include <stdlib.h>
+#include <string.h>
 
 #include "ptrdict.h"
 
